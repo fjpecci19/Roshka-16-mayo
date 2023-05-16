21eddf07882eb51af0dbc2af6a1ac28a5b8d6550
Franco Pecci <fapeccisan@gmail.com>
2023-05-16 16:40:57 -0400

    first commit

---
+++ FrontEnd.js
@@ -0,0 +1,15 @@
+const tipo = document.getElementById("Documento")
+const num = document.getElementById("docu")
+
+num.addEventListener("input", () => {
+    if (tipo.value == "ruc"){
+        let input = num.value
+        console.log(input)
+        const pattern = /^\d+-\d{1}$/
+        if (pattern.test(input)){
+            num.removeAttribute("style")
+        }else{
+            num.setAttribute("style","border:1px solid red; background-color: rgba(251, 54, 54, 0.778)")
+        }
+    }
+})
