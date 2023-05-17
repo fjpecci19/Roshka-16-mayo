82d4b0fed3ae3e97ee6d3f015abcba950a981be1
Franco Pecci <fapeccisan@gmail.com>
2023-05-17 11:37:02 -0400

    ...

--- TablaVersiones.js
+++ TablaVersiones.js
@@ -1,5 +1,23 @@
-function mostrar(){
+function mostrar1(){
     var textoculto = document.getElementById("archivoculto")
+    if(textoculto.style.display === "none"){
+        textoculto.style.display = "block"
+    }else{
+        textoculto.style.display = "none"
+    }
+}
+
+function mostrar2(){
+    var textoculto = document.getElementById("archivoculto2")
+    if(textoculto.style.display === "none"){
+        textoculto.style.display = "block"
+    }else{
+        textoculto.style.display = "none"
+    }
+}
+
+function mostrar3(){
+    var textoculto = document.getElementById("archivoculto3")
     if(textoculto.style.display === "none"){
         textoculto.style.display = "block"
     }else{
